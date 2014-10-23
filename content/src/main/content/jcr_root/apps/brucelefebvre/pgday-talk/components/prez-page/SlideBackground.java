a2f7408d9b003eb511caafe673e7eb76497b539e
Bruce Lefebvre <blefebvr@adobe.com>
2014-10-22 17:36:39 -0700

    slide template reworked to reduce duplication

--- content/src/main/content/jcr_root/apps/brucelefebvre/pgday-talk/components/prez-page/SlideBackground.java
+++ content/src/main/content/jcr_root/apps/brucelefebvre/pgday-talk/components/prez-page/SlideBackground.java
@@ -3,6 +3,7 @@ package apps.brucelefebvre.pgday_talk.components.prez_page;
 import com.adobe.cq.sightly.WCMUse;
 import com.adobe.cq.mobile.angular.data.util.FrameworkContentExporterUtils;
 import org.apache.sling.api.resource.Resource;
+import com.day.cq.wcm.api.Page;
   
 public class SlideBackground extends WCMUse {
 
@@ -10,17 +11,19 @@ public class SlideBackground extends WCMUse {
     boolean appExport;
     Resource imageResource;
     String imageSrc;
+    Page page;
 
     @Override
     public void activate() throws Exception {
-        topLevelAppResource = FrameworkContentExporterUtils.getTopLevelAppResource(getCurrentPage().adaptTo(Resource.class));
+        page = get("page", Page.class);
+        topLevelAppResource = FrameworkContentExporterUtils.getTopLevelAppResource(page.adaptTo(Resource.class));
         appExport = Boolean.parseBoolean(getRequest().getParameter("appExport"));
-        imageResource = getCurrentPage().getContentResource("image");
+        imageResource = page.getContentResource("image");
     }
 
     public String getRelPathToPageImage() {
         if (imageResource != null) {
-            imageSrc = getCurrentPage().getPath() + ".img.png";
+            imageSrc = page.getPath() + ".img.png";
             imageSrc = FrameworkContentExporterUtils.getPathToAsset(topLevelAppResource, imageSrc, appExport);
             
             return imageSrc;
