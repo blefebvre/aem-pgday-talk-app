a56c3a64828d970a9a752c08a1b6b5b533293ead
Bruce Lefebvre <blefebvr@adobe.com>
2016-01-28 14:10:19 -0500

    Targeting slide added
    Helper component updated to use age

--- content/src/main/content/jcr_root/etc/designs/phonegap/brucelefebvre/pgday-talk/clientlibsall/js/pgday-target.js
+++ content/src/main/content/jcr_root/etc/designs/phonegap/brucelefebvre/pgday-talk/clientlibsall/js/pgday-target.js
@@ -1,20 +1,36 @@
 window.pgday = window.pgday || {};
 
+pgday.targetAge = function(age) {
+    console.log('Targeting with age: ' + age);
+	pgday.targetContentFromProfile(
+        {
+            'profile.age': age
+        }
+    );
+};
+
 pgday.targetGender = function(gender) {
-    // Find all elements with a cq-targeting attribute
+    console.log('Targeting with gender: ' + gender);
+	pgday.targetContentFromProfile(
+        {
+            'profile.gender': gender
+        }
+    );
+};
+
+pgday.targetContentFromProfile = function(profile) {
+	// Find all elements with a cq-targeting attribute
     var targetedElements = document.querySelectorAll('[cq-targeting]');
 
     for (var i = 0; i < targetedElements.length; i++) {
         var targetedEl = targetedElements[i];
         var mboxId = targetedEl.dataset.mboxid;
-        console.log('Fetching targeted content for: [' + targetedEl + '], mboxid: [' + mboxId + '], with gender: [' + gender + '].');
+        console.log('Fetching targeted content for: [' + targetedEl + '], mboxid: [' + mboxId + '], with profile: [' + profile + '].');
 
         var target = new cq.mobileapps.targeting.Target(mboxId, targetedEl);
 
         target.targetLoadRequest(
-            {
-                'profile.gender': gender
-            }, 
+            profile,
             function(error, offer){
                 if (error) {
                     return console.error('error targeting: ' + error);
